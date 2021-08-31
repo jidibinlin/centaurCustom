405f3bb6eaaae96d94220e1a604d75552a7432e4
jidibinlin <2694273649@qq.com>
2021-08-31 21:09:46 +0800

    windows config

---
+++ custom-post.el
@@ -0,0 +1,58 @@
+(setq evil-want-integration t)
+(setq evil-want-keybinding nil)
+(require 'evil)
+(when (require 'evil-collection nil) t
+      (evil-collection-init))
+(evil-mode 1)
+
+(with-eval-after-load 'evil
+  (with-eval-after-load 'company
+    (define-key evil-insert-state-map (kbd "C-n") nil)
+    (define-key evil-insert-state-map (kbd "C-p") nil)
+    (evil-define-key nil company-active-map (kbd "C-n") #'company-select-next)
+    (evil-define-key nil company-active-map (kbd "C-p") #'company-select-previous)
+    (evil-define-key nil company-active-map (kbd "C-j") #'company-select-next)
+    (evil-define-key nil company-active-map (kbd "C-k") #'company-select-previous)
+    ))
+
+(require 'evil-org)
+(add-hook 'org-mode-hook 'evil-org-mode)
+(evil-org-set-key-theme '(navigation insert textobjects additional calendar))
+(require 'evil-org-agenda)
+(evil-org-agenda-set-keys)
+
+(setq org-latex-create-formula-image-program 'dvisvgm)
+
+(defun my-org-latex-yas()
+  (yas-minor-mode)
+  (yas-activate-extra-mode 'latex-mode))
+(add-hook 'org-mode-hook #'my-org-latex-yas)
+
+
+
+(use-package org-latex-impatient
+  :defer t
+  :hook (org-mode . org-latex-impatient-mode)
+  :init
+  (setq org-latex-impatient-tex2svg-bin
+        ;; location of tex2svg executable
+        "/lib/node_modules/mathjax-node-cli/bin/tex2svg"))
+
+(use-package rime
+  :custom
+  (default-input-method "rime"))
+(setq rime-show-candidate 'posframe)
+(define-key rime-mode-map (kbd "M-j") 'rime-force-enable)
+
+(setq rime-disable-predicates
+      '(rime-predicate-evil-mode-p
+        rime-predicate-after-alphabet-char-p
+        rime-predicate-prog-in-code-p
+        rime-predicate-current-input-punctuation-p
+        ))
+(add-hook 'text-mode-hook
+          (lambda ()
+            (variable-pitch-mode 1)))
+
+(require 'org-bullets)
+(add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
---
+++ custom.el
@@ -0,0 +1,145 @@
+;;; custom.el --- user customization file    -*- lexical-binding: t no-byte-compile: t -*-
+;;; Commentary:
+;;;       Add or change the configurations in custom.el, then restart Emacs.
+;;;       Put your own configurations in custom-post.el to override default configurations.
+;;; Code:
+
+;; (setq centaur-logo nil)                        ; Logo file or nil (official logo)
+;; (setq centaur-full-name "user name")           ; User full name
+;; (setq centaur-mail-address "user@email.com")   ; Email address
+;; (setq centaur-proxy "127.0.0.1:1087")          ; HTTP/HTTPS proxy
+;; (setq centaur-socks-proxy "127.0.0.1:1086")    ; SOCKS proxy
+;; (setq centaur-server nil)                      ; Enable `server-mode' or not: t or nil
+;; (setq centaur-icon nil)                        ; Display icons or not: t or nil
+(setq centaur-package-archives 'tencent)         ; Package repo: melpa, emacs-china, netease, ustc, tencent or tuna
+(setq centaur-theme 'doom-monokai-classic)                     ; Color theme: auto, random, system, default, pro, dark, light, warm, cold, day or night
+;; (setq centaur-completion-style 'minibuffer)    ; Completion display style: minibuffer or childframe
+;; (setq centaur-dashboard nil)                   ; Use dashboard at startup or not: t or nil
+;; (setq centaur-restore-frame-geometry nil)      ; Restore the frame's geometry at startup: t or nil
+;; (setq centaur-lsp 'eglot)                      ; Set LSP client: lsp-mode, eglot or nil
+;; (setq centaur-lsp-format-on-save-ignore-modes '(c-mode c++-mode)) ; Ignore format on save for some languages
+(setq centaur-chinese-calendar nil)              ; Use Chinese calendar or not: t or nil
+;;(setq centaur-prettify-symbols-alist t)      ; Alist of symbol prettifications. Nil to use font supports ligatures.
+;;(setq centaur-prettify-org-symbols-alist t)  ; Alist of symbol prettifications for `org-mode'
+
+;; For Emacs devel
+;; (setq package-user-dir (locate-user-emacs-file (format "elpa-%s" emacs-major-version)))
+;; (setq desktop-base-file-name (format ".emacs-%s.desktop" emacs-major-version))
+;; (setq desktop-base-lock-name (format ".emacs-%s.desktop.lock" emacs-major-version))
+
+;; Fonts
+(when (display-graphic-p)
+  ;; Set default font
+  (cl-loop for font in '("SF Mono" "Hack" "Source Code Pro" "Fira Code"
+                         "Menlo" "Monaco" "DejaVu Sans Mono" "Consolas")
+           when (font-installed-p font)
+           return (set-face-attribute 'default nil
+                                      :font font
+                                      :height (cond (sys/mac-x-p 130)
+                                                    (sys/win32p 105)
+                                                    (t 105))))
+
+  ;; Specify font for all unicode characters
+  (cl-loop for font in '("Apple Color Emoji" "Segoe UI Symbol" "Symbola" "Symbol")
+           when (font-installed-p font)
+           return(set-fontset-font t 'unicode font nil 'prepend))
+
+  ;; Specify font for Chinese characters
+  (cl-loop for font in '("WenQuanYi Micro Hei" "Microsoft Yahei")
+           when (font-installed-p font)
+           return (set-fontset-font t '(#x4e00 . #x9fff) font)))
+
+;; Mail
+;; (setq message-send-mail-function 'smtpmail-send-it
+;;       smtpmail-starttls-credentials '(("smtp.gmail.com" 587 nil nil))
+;;       smtpmail-auth-credentials '(("smtp.gmail.com" 587
+;;                                    user-mail-address nil))
+;;       smtpmail-default-smtp-server "smtp.gmail.com"
+;;       smtpmail-smtp-server "smtp.gmail.com"
+;;       smtpmail-smtp-service 587)
+
+;; Calendar
+;; Set location , then press `S' can show the time of sunrise and sunset
+;; (setq calendar-location-name "Chengdu"
+;;       calendar-latitude 30.67
+;;       calendar-longitude 104.07)
+
+;; Misc.
+;; (setq confirm-kill-emacs 'y-or-n-p)
+
+;; Enable proxy
+;; (proxy-http-enable)
+;; (proxy-socks-enable)
+
+;; Display on the specified monitor
+;; (when (and (> (length (display-monitor-attributes-list)) 1)
+;;            (> (display-pixel-width) 1920))
+;;   (set-frame-parameter nil 'left 1920))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(doom-modeline-buffer-file-name-style 'file-name)
+ '(doom-modeline-height 15)
+ '(warning-suppress-log-types '((comp) (comp) (comp) (comp) (comp) (comp)))
+ '(warning-suppress-types '((comp) (comp) (comp) (comp) (comp))))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(aw-leading-char-face ((t (:inherit font-lock-keyword-face :bold t :height 2.0))))
+ '(aw-minibuffer-leading-char-face ((t (:inherit font-lock-keyword-face :bold t :height 1.0))))
+ '(aw-mode-line-face ((t (:inherit mode-line-emphasis :bold t))))
+ '(dashboard-heading ((t (:inherit (font-lock-string-face bold)))))
+ '(diff-hl-change ((t (:foreground "#FD971F" :background nil))))
+ '(diff-hl-delete ((t (:inherit diff-removed :background nil))))
+ '(diff-hl-insert ((t (:inherit diff-added :background nil))))
+ '(doom-modeline-buffer-file ((t (:inherit (mode-line bold)))))
+ '(flycheck-posframe-background-face ((t (:inherit tooltip))))
+ '(flycheck-posframe-border-face ((t (:inherit font-lock-comment-face))))
+ '(flycheck-posframe-face ((t (:foreground "#A6E22E"))))
+ '(flycheck-posframe-info-face ((t (:foreground "#A6E22E"))))
+ '(git-timemachine-minibuffer-author-face ((t (:inherit success))))
+ '(git-timemachine-minibuffer-detail-face ((t (:inherit warning))))
+ '(ivy-minibuffer-match-face-1 ((t (:foreground "#7f7f80"))))
+ '(ivy-posframe ((t (:inherit tooltip))))
+ '(ivy-posframe-border ((t (:background "#555556"))))
+ '(lsp-headerline-breadcrumb-path-error-face ((t :underline (:style wave :color "#E74C3C") :inherit lsp-headerline-breadcrumb-path-face)))
+ '(lsp-headerline-breadcrumb-path-hint-face ((t :underline (:style wave :color "#A6E22E") :inherit lsp-headerline-breadcrumb-path-face)))
+ '(lsp-headerline-breadcrumb-path-info-face ((t :underline (:style wave :color "#A6E22E") :inherit lsp-headerline-breadcrumb-path-face)))
+ '(lsp-headerline-breadcrumb-path-warning-face ((t :underline (:style wave :color "#E6DB74") :inherit lsp-headerline-breadcrumb-path-face)))
+ '(lsp-headerline-breadcrumb-symbols-error-face ((t :inherit lsp-headerline-breadcrumb-symbols-face :underline (:style wave :color "#E74C3C"))))
+ '(lsp-headerline-breadcrumb-symbols-hint-face ((t :inherit lsp-headerline-breadcrumb-symbols-face :underline (:style wave :color "#A6E22E"))))
+ '(lsp-headerline-breadcrumb-symbols-info-face ((t :inherit lsp-headerline-breadcrumb-symbols-face :underline (:style wave :color "#A6E22E"))))
+ '(lsp-headerline-breadcrumb-symbols-warning-face ((t :inherit lsp-headerline-breadcrumb-symbols-face :underline (:style wave :color "#E6DB74"))))
+ '(lsp-ui-sideline-code-action ((t (:inherit warning))))
+ '(macrostep-expansion-highlight-face ((t (:inherit tooltip :extend t))))
+ '(org-ellipsis ((t (:foreground nil))))
+ '(org-pomodoro-mode-line ((t (:inherit warning))))
+ '(org-pomodoro-mode-line-break ((t (:inherit success))))
+ '(org-pomodoro-mode-line-overtime ((t (:inherit error))))
+ '(org-table ((t (:foreground "#6c71c4" :family "Sarasa Mono SC Nerd"))))
+ '(pulse-highlight-face ((t (:inherit region))))
+ '(pulse-highlight-start-face ((t (:inherit region))))
+ '(symbol-overlay-default-face ((t (:inherit (region bold)))))
+ '(transient-posframe ((t (:inherit tooltip))))
+ '(transient-posframe-border ((t (:background "#555556"))))
+ '(which-key-posframe ((t (:inherit tooltip))))
+ '(which-key-posframe-border ((t (:background "#555556"))))
+ '(ztreep-arrow-face ((t (:inherit font-lock-comment-face))))
+ '(ztreep-diff-header-face ((t (:inherit (diff-header bold)))))
+ '(ztreep-diff-header-small-face ((t (:inherit diff-file-header))))
+ '(ztreep-diff-model-add-face ((t (:inherit diff-nonexistent))))
+ '(ztreep-diff-model-diff-face ((t (:inherit diff-removed))))
+ '(ztreep-diff-model-ignored-face ((t (:inherit font-lock-doc-face :strike-through t))))
+ '(ztreep-diff-model-normal-face ((t (:inherit font-lock-doc-face))))
+ '(ztreep-expand-sign-face ((t (:inherit font-lock-function-name-face))))
+ '(ztreep-header-face ((t (:inherit diff-header))))
+ '(ztreep-leaf-face ((t (:inherit diff-index))))
+ '(ztreep-node-face ((t (:inherit font-lock-variable-name-face)))))
+
+;;; custom.el ends here
