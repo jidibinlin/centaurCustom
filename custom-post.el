6e358360442e2136d7dfb49d38f7c51ae0036a34
jidibinlin <2694273649@qq.com>
2021-10-16 15:25:23 +0800

    fix bug

--- custom-post.el
+++ custom-post.el
@@ -4,12 +4,12 @@
 (when (require 'evil-collection nil) t
       (evil-collection-init))
 (evil-mode 1)
+
 (evil-set-leader 'normal (kbd "SPC"))
 
 (evil-define-key 'normal 'global (kbd "<leader>bb") 'ivy-switch-buffer)
 
 (evil-define-key 'normal 'global (kbd "<leader>f") 'find-file)
-
 (with-eval-after-load 'evil
   (with-eval-after-load 'company
     (define-key evil-insert-state-map (kbd "C-n") nil)
@@ -33,15 +33,23 @@
   (yas-activate-extra-mode 'latex-mode))
 (add-hook 'org-mode-hook #'my-org-latex-yas)
 
+(setq company-minimum-prefix-length 3
+      company-idle-delay 0.5)
+;;(setq gc-cons-threshold 100000000)
+(setq read-process-output-max (*1024 1024))
+(setq lsp-idle-delay 0.5)
+(setq lsp-ui-sideline-delay 0.5)
+(setq lsp-ui-doc-delay 0.5)
 
 
-(use-package org-latex-impatient
-  :defer t
-  :hook (org-mode . org-latex-impatient-mode)
-  :init
-  (setq org-latex-impatient-tex2svg-bin
-        ;; location of tex2svg executable
-        "/lib/node_modules/mathjax-node-cli/bin/tex2svg"))
+
+;; (use-package org-latex-impatient
+;;   :defer t
+;;   :hook (org-mode . org-latex-impatient-mode)
+;;   :init
+;;   (setq org-latex-impatient-tex2svg-bin
+;;         ;; location of tex2svg executable
+;;         "/lib/node_modules/mathjax-node-cli/bin/tex2svg"))
 
 
 (require 'org-bullets)
@@ -52,6 +60,8 @@
 (global-set-key (kbd "C-<left>")  'centaur-tabs-backward)
 (global-set-key (kbd "C-<right>") 'centaur-tabs-forward)
 ;;(desktop-save-mode t)
+
+
 
 
 
