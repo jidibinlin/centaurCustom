d94d6bb01731d10d23db49adb235bfa6f5f19688
jidibinlin <2694273649@qq.com>
2023-05-27 14:53:38 +0800

    fix

--- custom.el
+++ custom.el
@@ -1,5 +1,5 @@
-;;; custom.el --- user customization file    -*- lexical-binding: t no-byte-compile: t -*-
 ;;; Commentary:
+;;; custom.el --- user customization file    -*- lexical-binding: t no-byte-compile: t -*-
 ;;;       Add or change the configurations in custom.el, then restart Emacs.
 ;;;       Put your own configurations in custom-post.el to override default configurations.
 ;;; Code:
@@ -11,25 +11,27 @@
 ;;(setq centaur-proxy "127.0.0.1:1087")          ; HTTP/HTTPS proxy
 (setq centaur-socks-proxy "127.0.0.1:7890")    ; SOCKS proxy
 (setq centaur-server t)                      ; Enable `server-mode' or not: t or nil
-;;(setq centaur-icon nil)                        ; Display icons or not: t or nil
-(setq centaur-package-archives 'melpa)         ; Package repo: melpa, emacs-china, netease, ustc, tencent or tuna
-(setq centaur-theme 'system)                     ; Color theme: auto, random, system, default, pro, dark, light, warm, cold, day or night
+(setq centaur-icon t)                        ; Display icons or not: t or nil
+(setq centaur-package-archives 'ustc)         ; Package repo: melpa, emacs-china, netease, ustc, tencent or tuna
+(setq centaur-theme 'night-tinted)                     ; Color theme: auto, random, system, default, pro, dark, light, warm, cold, day or night
 (setq centaur-completion-style 'childframe)    ; Completion display style: minibuffer or childframe
 ;; (setq centaur-dashboard nil)                   ; Use dashboard at startup or not: t or nil
 (setq centaur-restore-frame-geometry t)      ; Restore the frame's geometry at startup: t or nil
-(setq centaur-lsp nil)
+(setq centaur-lsp 'eglot)
                                         ; Set LSP client: lsp-mode, eglot or nil
 ;; (setq centaur-lsp-format-on-save-ignore-modes '(c-mode c++-mode)) ; Ignore format on save for some languages
 (setq centaur-tree-sitter t)
 (setq centaur-chinese-calendar nil)              ; Use Chinese calendar or not: t or nil
-(setq centaur-search-backend "ivy")
 ;;(setq centaur-prettify-symbols-alist nil)      ; Alist of symbol prettifications. Nil to use font supports ligatures.
 ;; (setq centaur-prettify-org-symbols-alist '()) ; Alist of symbol prettifications for `org-mode'
+
+(setq centaur-search 'vertico)
 
 (setq centaur-blur t)
 (setenv "INFOPATH" "/opt/homebrew/share/info")
 (setenv "LSP_USE_PLISTS" "true")
 (setenv "DOTNET_ROOT" "/usr/local/share/dotnet/")
+(set-face-attribute 'font-lock-keyword-face nil :slant 'italic)
 
 ;; For Emacs devel
 ;; (setq package-user-dir (locate-user-emacs-file (format "elpa-%s" emacs-major-version)))
@@ -38,11 +40,11 @@
 ;; Fonts
 (when (display-graphic-p)
   ;; Set default font
-  (cl-loop for font in '("DejaVuSansMono Nerd Font" "Monaco" "SFMono Nerd Font Mono" "SFMono Nerd Font" "DejaVu Sans Code" "MonegoLigatures Nerd Font" "Hack" "Source Code Pro" "Fira Code" "Menlo" "Consolas")
+  (cl-loop for font in '("Monaco" "DejaVuSansMono Nerd Font" "SFMono Nerd Font Mono" "SFMono Nerd Font" "DejaVu Sans Code" "MonegoLigatures Nerd Font" "Hack" "Source Code Pro" "Fira Code" "Menlo" "Consolas")
            when (font-installed-p font)
            return (set-face-attribute 'default nil
                                       :family font
-                                      :height (cond (sys/mac-x-p 130)
+                                      :height (cond (sys/mac-x-p 115)
                                                     (sys/win32p 105)
                                                     (t 100))))
 
@@ -96,8 +98,8 @@
  ;; If there is more than one, they won't work right.
  '(auth-source-save-behavior nil)
  '(vc-follow-symlinks nil)
- '(warning-suppress-types '((use-package)))
- )
+ '(warning-suppress-types '((use-package))))
+
 
 ;;; custom.el ends here
 
@@ -107,7 +109,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  ;;'(font-lock-keyword-face ((t (:slant italic :family "UbuntuMono Nerd Font" :height 180 :bold t))))
- '(font-lock-keyword-face ((t (:slant italic :family "UbuntuMono Nerd Font" :height 170 :bold nil))))
+ ;; '(font-lock-keyword-face ((t (:slant italic :family "UbuntuMono Nerd Font" :height 150 :bold nil))))
  '(doom-modeline-evil-insert-state ((t (:inherit doom-modeline-evil-normal-state :foreground "#F92660" ))))
  ;;'(marginalia-date ((t (:inherit font-lock-keyword-face :family "SFMono Nerd Font" :slant normal :height 150))))
  )
+
+(load "~/.centaurCustom/luna.el")
--- luna.el
+++ luna.el
@@ -1,171 +1,109 @@
-(defun mkluna (&optional dont-focus)
-  "build luna"
-  (interactive)
-  (unless dont-focus
-    (switch-to-buffer "*Messages*" nil t)
-    (end-of-buffer))
-  (start-process "luna_releaser" (get-buffer "*Messages*")
-                 "luna_releaser" "game"
-                 "--install" "/opt/luna_server/game/game"
-                 "--project_root" "~/Data/luna_game/"))
-(defun mkluna-full (&optional dont-focus)
-  "build luna with export config generate model and proto"
-  (unless dont-focus
-    (switch-to-buffer "*Messages*" nil t)
-    (end-of-buffer))
-  (start-process "luna_releaser" (get-buffer "*Messages*")
-                 "luna_releaser" "game"
-                 "--client" "~/Data/luna/master/"
-                 "--export_proto"
-
-                 "--gen_model"
-
-                 "--word" "~/Data/luna_word/luna_word_master/"
-                 "--data" "/opt/cycle_test/game/data/"
-                 "--word_tool" "~/Data/luna_export_tool/"
-
-                 "--install" "/opt/luna_server/game/game"
-                 "--project_root" "~/Data/luna_game/"))
-
-(defun mkluna-with-export-config (&optional dont-focus)
-  "build luna with export config"
-  (interactive)
-  (unless dont-focus
-    (switch-to-buffer "*Messages*" nil t)
-    (end-of-buffer))
-  (start-process "luna_releaser" (get-buffer "*Messages*")
-                 "luna_releaser" "game"
-                 "--word" "~/Data/luna_word/luna_word_master/"
-                 "--data" "/opt/cycle_test/game/data/"
-                 "--word_tool" "~/Data/luna_export_tool/"
-                 "--install" "/opt/luna_server/game/game"
-                 "--project_root" "~/Data/luna_game/"))
-
-(defun mkluna-with-gen-proto (&optional dont-focus)
-  "build luna with gen proto"
-  (interactive)
-  (unless dont-focus
-    (switch-to-buffer "*Messages*" nil t)
-    (end-of-buffer))
-  (start-process "luna_releaser" (get-buffer "*Messages*")
-                 "luna_releaser" "game"
-                 "--client" "~/Data/luna/master/"
-                 "--export_proto"
-                 "--install" "/opt/luna_server/game/game"
-                 "--project_root" "~/Data/luna_game/"))
-
-(defun luna-genproto-cross-guild (&optional dont-focus)
-  "generate proto specially for cross-guild branch"
-  (interactive)
-  (unless dont-focus
-    (switch-to-buffer "*Messages*" nil t)
-    (end-of-buffer))
-  (start-process "luna_releaser" (get-buffer "*Messages*")
-                 "luna_releaser" "game"
-                 "--client" "~/Data/luna/master_guild/"
-                 "--export_proto"
-                 "--install" "/opt/luna_server/game/game"
-                 "--project_root" "~/Data/luna_game/"))
-
-(defun mkluna-with-gen-model (&optional dont-focus)
-  "build luna with gen model"
-  (interactive)
-  (unless dont-focus
-    (switch-to-buffer "*Messages*" nil t)
-    (end-of-buffer))
-  (start-process "luna_releaser" (get-buffer "*Messages*")
-                 "luna_releaser" "game"
-                 "--gen_model"
-                 "--install" "/opt/luna_server/game/game"
-                 "--project_root" "~/Data/luna_game/"))
-
-(defun luna-genproto (&optional dont-focus)
-  "build luna with gen proto"
-  (interactive)
-  (unless dont-focus
-    (switch-to-buffer "*Messages*" nil t)
-    (end-of-buffer))
-  (start-process "luna_releaser" (get-buffer "*Messages*")
-                 "luna_releaser" "proto"
-                 "--client" "~/Data/luna/master/"
-                 "--game_root" "~/Data/luna_game/"))
-
-(defun luna-genproto-for-test (&optional dont-focus)
-  (interactive)
-  (unless dont-focus
-    (switch-to-buffer "*Messages*" nil t)
-    (end-of-buffer))
-  (start-process "luna_releaser" (get-buffer "*Messages*")
-                 "protoc" "-I=/Users/qibinyang/go/pkg/mod/github.com/envoyproxy/protoc-gen-validate@v0.6.7/"
-                 "--plugin=protoc-gen-cl-pb=/usr/local/bin/protoc-gen-cl-pb"
-                 "--cl-pb_out=output-file=game.lisp:/Users/qibinyang/Documents/luna_test/"
-                 "--proto_path=/Users/qibinyang/Data/luna/master/tools/protobuf_cs/proto/"
-                 "game.proto"))
-
-(defun luna-genmodel(&optional dont-focus)
-  "build luna with gen model"
-  (interactive)
-  (unless dont-focus
-    (switch-to-buffer "*Messages*" nil t)
-    (end-of-buffer))
-  (start-process "luna_releaser" (get-buffer "*Messages*")
-                 "luna_releaser" "model"
-                 "--project_root" "~/Data/luna_game/"))
-
-(defun luna-export-config(&optional dont-focus)
-  "build luna with export config"
-  (interactive)
-  (unless dont-focus
-    (switch-to-buffer "*Messages*" nil t)
-    (end-of-buffer))
-  (start-process "luna_releaser" (get-buffer "*Messages*")
-                 "luna_releaser" "word"
-                 "--data" "/opt/luna_server/game/data/"
-                 "--game_root" "~/Data/luna_game/"
-                 "--tool" "~/Data/luna_export_tool/"
-                 "--word" "~/Data/luna_word/luna_word_master/"))
 
 
-(setq luna_time "@2022-11-16 23:59:00")
-(setq luna_run "/Users/qibinyang/Data/luna_time_test/luna_time_tast.ros")
-(defun cycle-rank-test()
+(defun ansi-color-after-scroll (window start)
+  "Used by ansi-color-mode minor mode"
+  (ansi-color-apply-on-region start (window-end window t) t))
+
+(define-minor-mode ansi-color-mode
+  "A very primitive minor mode to view log files containing ANSI color codes.
+
+Pros: this minor mode runs `ansi-color-apply-on-region' lazily,
+i.e. only the visible part of the buffer. Hence, it does NOT
+freeze Emacs even if the log file is huge.
+
+Cons: a) when the minor code is toggled off, it does not undo
+what has already been ansi colorized. b) assumes the buffer
+content etc. does not change. c) jumping to random places within
+the buffer may incur incorrect/incomplete colorization.
+
+How to install: put this code into your init.el, then evaluate it or
+restart Emacs for the code to take effect.
+
+How to use: in the log buffer of need run `M-x ansi-color-mode'.
+Alternatively, feel free to enable this minor mode via mode hooks
+so that you needn't enable it manually.
+
+-- lgfang
+"
+  :global nil
+  :lighter ""
+  (when ansi-color-mode
+    (progn
+      ;; (ansi-color-apply-on-region (window-start) (window-end) t)
+      ;; (add-hook 'window-scroll-functions 'ansi-color-after-scroll 80 t)
+      (add-to-list 'after-change-functions 'ansi-color-apply-on-region)
+      )))
+
+(setq faketime "@2023-5-25 23:59:30")
+(setq faketimetool "faketimer")
+(setq faketimelibpath "/Users/qibinyang/Downloads/libfaketime/src/libfaketime.1.dylib")
+(setq gameserver-path "/opt/jiugong/gameserver")
+(setq fightserver-path "/opt/jiugong/fightsrv")
+
+(defun restart-u3dgame-faketime ()
   "test luna game"
   (interactive)
-  (kill-luna)
-  (sleep-for 2)
-  (start-process "run_game" (get-buffer "*Messages*") luna_run "-e" "/opt/cycle_test/game/game" "-t" luna_time)
-  (start-process-shell-command "cycle_rank_test" (get-buffer "*Messages*") "alacritty -e /opt/cycle_test/game/makeGame.sh")
-
-  (start-process "run_game" (get-buffer "*Messages*") luna_run "-e" "/opt/cycle_test/game2/game" "-t" luna_time)
-  (start-process-shell-command "cycle_rank_test2" (get-buffer "*Messages*") "alacritty -e /opt/cycle_test/game2/makeGame.sh")
-
-  (start-process "run_game" (get-buffer "*Messages*") luna_run "-e" "/opt/cycle_test/game_center/game" "-t" luna_time)
-  (start-process-shell-command "cycle_rank_test_center" (get-buffer "*Messages*") "alacritty -e /opt/cycle_test/game_center/makeGame.sh")
+  (kill-u3dgame)
+  (start-process "u3dgame" nil "alacritty" "-e" faketimetool "-e" gameserver-path "-t" faketime "-l" faketimelibpath)
   )
 
-(defun cycle-rank-test-now ()
+(defun  restart-u3dgame ()
   "test luna game base on the time of now"
   (interactive)
-  (kill-luna)
-  (sleep-for 2)
-  (setq luna_time (format "@%s" (shell-command-to-string "echo -n $(date +%Y-%m-%d\\ %H:%M:%S)")))
-  (start-process "run_game" (get-buffer "*Messages*") luna_run "-e" "/opt/cycle_test/game/game" "-t" luna_time)
-  (start-process-shell-command "cycle_rank_test" (get-buffer "*Messages*") "alacritty -e /opt/cycle_test/game/makeGame.sh")
 
-  (start-process "run_game" (get-buffer "*Messages*") luna_run "-e" "/opt/cycle_test/game2/game" "-t" luna_time)
-  (start-process-shell-command "cycle_rank_test2" (get-buffer "*Messages*") "alacritty -e /opt/cycle_test/game2/makeGame.sh")
+  (kill-u3dgame)
+  (let ((buffer (get-buffer-create "*u3dgame*"))
+        (curtime (format-time-string "@%Y-%m-%d %H:%M:%S" (current-time))))
 
-  (start-process "run_game" (get-buffer "*Messages*") luna_run "-e" "/opt/cycle_test/game_center/game" "-t" luna_time)
-  (start-process-shell-command "cycle_rank_test_center" (get-buffer "*Messages*") "alacritty -e /opt/cycle_test/game_center/makeGame.sh")
+    (start-process "u3dgame" nil "alacritty" "-e" faketimetool "-e" gameserver-path "-t" curtime "-l" faketimelibpath)
+    )
   )
 
+(defun restart-u3dfight-faketime ()
+  "test luna game"
+  (interactive)
 
-(defun kill-luna ()
+  (kill-u3dfight)
+  (start-process "u3dfight" nil "alacritty" "-e" faketimetool "-e" fightserver-path "-t" faketime "-l" faketimelibpath))
+
+(defun  restart-u3dfight ()
+  "test luna game base on the time of now"
+  (interactive)
+
+  (kill-u3dfight)
+  (let ((buffer (get-buffer-create "*u3dfight*"))
+        (curtime (format-time-string "@%Y-%m-%d %H:%M:%S" (current-time))))
+    (start-process "u3dfight" nil "alacritty" "-e" faketimetool "-e" fightserver-path "-t" curtime "-l" faketimelibpath)))
+
+(defun restart-u3dserver()
+  (interactive)
+  (kill-u3dserver)
+  (let ((buffer (get-buffer-create "*u3dfight*"))
+        (curtime (format-time-string "@%Y-%m-%d %H:%M:%S" (current-time))))
+    (start-process "u3dfight" nil "alacritty" "-e" faketimetool "-e" fightserver-path "-t" curtime "-l" faketimelibpath)
+    (start-process "u3dgame" nil "alacritty" "-e" faketimetool "-e" gameserver-path "-t" curtime "-l" faketimelibpath)))
+
+(defun restart-u3dserver-faketime ()
+  "test luna game"
+  (interactive)
+  (kill-u3dserver)
+  (start-process "u3dgame" nil "alacritty" "-e" faketimetool "-e" gameserver-path "-t" faketime "-l" faketimelibpath)
+  (start-process "u3dfight" nil "alacritty" "-e" faketimetool "-e" fightserver-path "-t" faketime "-l" faketimelibpath))
+
+
+(defun kill-u3dserver ()
   "kill all luna processes"
   (interactive)
-  (start-process-shell-command "kill_luna" (get-buffer "*Messages*") "ps aux | awk '/[g]ame/ {print $2}' | xargs kill -9")
-  (start-process-shell-command "kill_proxy" (get-buffer "*Message*") "ps aux | awk '/[p]roxy/ {print $2}' | xargs kill -9")
-  )
+  (call-process-shell-command "kill_gamesrv")
+  (call-process-shell-command "kill_fightsrv"))
+
+(defun kill-u3dgame ()
+  (interactive)
+  (call-process-shell-command "kill_gamesrv" nil nil))
+
+(defun kill-u3dfight()
+  (interactive)
+  (call-process-shell-command "kill_fightsrv"))
 
 (setq luna-word-directory "~/Data/luna_word/luna_word_master/99_配置表/excel_config/")
 
@@ -175,12 +113,3 @@
   (let ((default-directory luna-word-directory))
     (call-interactively #'counsel-find-file-extern))
   )
-
-(defun open-proto-xlsx ()
-  "Open proto explain filed for luna project"
-  (interactive)
-  (call-process "open" nil 0 nil (expand-file-name "~/Data/luna/master/tools/protobuf_cs/协议说明.xlsx")))
-
-(defun open-proto-file ()
-  (interactive)
-  (find-file (expand-file-name "~/Data/luna/master/tools/protobuf_cs/proto/game.proto")))
